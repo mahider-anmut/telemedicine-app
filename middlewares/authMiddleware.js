4a9cf0c03d115066222032da845d741165674c98
Mahider <mahideranmut@gmail.com>
2025-05-21 14:33:43 +0200

    add middleware

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,62 @@
+let jwt = require('jsonwebtoken');
+
+let isAuthenticated = (req,res,next) => {
+	
+	let bearerHeader = req.headers["authorization"];
+
+	if(typeof bearerHeader !== 'undefined'){
+		let bearer = bearerHeader.split(" ");
+		let bearerToken = bearer[1];
+		jwt.verify(bearerToken,config.EnVar.JWT_KEY,function(err,pass){
+			if(err){
+				res.status(401).send("Authentication Failed");
+			}else{
+				req.id = pass.id;
+				req.role = pass.role;
+				req.token = bearerToken
+				next();
+		    }
+	  	});
+	}else{
+		res.status(401).send("Authentication Failed");
+	}
+}
+
+let isUser = (req, res, next) => {
+	if (req.id == req.params.userId){
+		next();
+	}else{
+		res.status(403).send("Invalid User Token");
+	}
+}
+
+let isAdmin = (req, res, next) => {
+	if (req.role == "admin") {
+		next();
+	} else {
+		res.status(403).send("Role Not Authorized");
+	}
+}
+
+let isPatient = (req, res, next) => {
+	if (req.role == "patient") {
+		next();
+	} else {
+		res.status(403).send("Role Not Authorized");
+	}
+}
+
+let isDoctor = (req, res, next) => {
+	if (req.role == "doctor") {
+		next();
+	} else {
+		res.status(403).send("Role Not Authorized");
+	}
+}
+
+
+module.exports.isAuthenticated = isAuthenticated;
+module.exports.isUser = isUser;
+module.exports.isPatient = isPatient;
+module.exports.isAdmin = isAdmin;
+module.exports.isDoctor = isDoctor;
