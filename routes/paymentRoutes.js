4c6cdfa38a8ecfcf4e3aaf679cf154c7752ff03d
Mahider <mahideranmut@gmail.com>
2025-05-27 08:16:27 +0200

    fix callback call

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -6,10 +6,10 @@ const { initiatePayment,paymentCallback,paymentStatusByAppointmentId,paymentStat
 const router = express.Router();
 
 
-router.post('/initiate', initiatePayment);
+router.post('/initiate',auth.isAuthenticated, initiatePayment);
 router.post('/callback', paymentCallback);
-router.get('/check-status/appointment/:id', paymentStatusByAppointmentId);
-router.get('/check-status/transaction/:id', paymentStatusByTransactionId);
-router.get('/check-status/user/:id', paymentStatusByUserId);
+router.get('/check-status/appointment/:id',auth.isAuthenticated, paymentStatusByAppointmentId);
+router.get('/check-status/transaction/:id',auth.isAuthenticated, paymentStatusByTransactionId);
+router.get('/check-status/user/:id',auth.isAuthenticated, paymentStatusByUserId);
 
 module.exports = router;
